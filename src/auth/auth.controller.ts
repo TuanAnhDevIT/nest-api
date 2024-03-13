785a4a39601d2fb301a5985a466e553262964e56
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2024-03-13 16:50:58 +0700

    feat(): tạo Prisma and định nghĩa DTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('/register')
-  register() {
+  @Post('register')
+  register(@Body('email') email: string, @Body('pass') password: string) {
+    console.log(`email = ${email}, password = ${password}`);
+    console.log(`type of email is: ${typeof email}`);
     return this.authService.register();
   }
 
-  @Post('login')
-  login() {
-    return this.authService.login();
-  }
+  //   @Post('login')
+  //   login() {
+  //     return this.authService.login();
+  //   }
 }
